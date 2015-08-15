9f9c4a8039233f27d88551634c449f63a5260816
Martins Olekss <martinso@scandiweb.com>
2015-08-16 00:16:57 +0300

    Add CND for jquery

--- index.php
+++ index.php
@@ -53,6 +53,7 @@ $app = new App();
                 </div>
             </div>
         </div>
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     </body>
 </html>
