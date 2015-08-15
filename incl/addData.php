a856a014a6011f482bcb3374c42f263554d7a65a
Martins Olekss <martinso@scandiweb.com>
2015-08-15 23:14:53 +0300

    Implement data insertion

--- incl/addData.php
+++ incl/addData.php
@@ -1,14 +1,21 @@
+<?php
+if (isset($_POST['add-data-submit']))
+{
+
+    $db->addData($_POST["dataContent"]);
+}
+?>
 <div class="row">
     <h2>Add data</h2>
 </div>
 <div class="row">
     <div class="col-sm-1"></div>
     <div class="col-sm-10">
-        <form>
+        <form action="" method="post">
             <div class="form-group">
                 <textarea name="dataContent" class="form-control" rows="3"></textarea>
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <button type="submit" name="add-data-submit" class="btn btn-default">Submit</button>
         </form>
     </div>
     <div class="col-sm-1"></div>
